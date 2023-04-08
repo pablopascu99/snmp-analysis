ecb3f24323576d61e7756515348f34f88495fbf9
pablopascu99 <pablopascu99@gmail.com>
2023-04-08 10:01:40 +0200

    Consumer. PostgreSQL and Grafana conexion established

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,7 +1,6 @@
 from confluent_kafka import Consumer
-from cassandra.cluster import Cluster
 import json
-import sys
+import psycopg2
 
 c = Consumer({'bootstrap.servers':'kafka1:19091','group.id':'counting-group','auto.offset.reset':'earliest'})
 print('Kafka Consumer has been initiated...')
@@ -9,21 +8,21 @@ print('Kafka Consumer has been initiated...')
 #print('Available topics to consume: ', c.list_topics().topics)
 c.subscribe(['user-tracker'])
 
-cluster = Cluster(contact_points=['cassandra'], port=9042)
-session = cluster.connect()
-session.execute("""
-    CREATE KEYSPACE IF NOT EXISTS snmp_traps
-    WITH REPLICATION =
-    { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };
-    """)
-session.execute("""
-    USE snmp_traps;
-    """)
-session.execute("""
-    CREATE TABLE IF NOT EXISTS usuarios (user_id int, user_name text, user_address text, platform text, PRIMARY KEY (user_id));
-    """)
-session.set_keyspace('snmp_traps')
+#establishing the connection
+conn = psycopg2.connect(database="postgres", user='postgres', password='postgres', host='postgres', port= '5432')
 
+#Creating a cursor object using the cursor() method
+cursor = conn.cursor()
+
+sql= '''CREATE TABLE IF NOT EXISTS users (
+    user_id INTEGER PRIMARY KEY,
+    user_name VARCHAR (255),
+    user_address VARCHAR (255),
+    platform VARCHAR (255)
+)'''
+
+cursor.execute(sql)
+conn.commit()
 def main():
 
     while True:
@@ -33,17 +32,16 @@ def main():
         if msg.error():
             print('Error: {}'.format(msg.error()))
             continue
+        #Setting auto commit false
+        conn.autocommit = True
+
+        #Creating a cursor object using the cursor() method
+        cursor = conn.cursor()
         data=json.loads(msg.value().decode('utf-8'))
-        
-        session.execute(
-            """
-            INSERT INTO usuarios (user_id, user_name, user_address, platform)
-            VALUES (%s, %s, %s, %s)
-            """,
+        cursor.execute(""" INSERT INTO users (user_id, user_name, user_address, platform) VALUES (%s,%s,%s,%s)""",
             (data['user_id'],
              data['user_name'],
              data['user_address'],
              data['platform']))
-    c.close()
 
 main()
